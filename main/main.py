285e9c20fddaa5e92f6542a4bdaea12ff9f020da
natashankaranja <natashankaranja@gmail.com>
2025-06-28 13:53:53 +0300

    added to main

--- main/main.py
+++ main/main.py
@@ -1,2 +1,15 @@
 from database.db import Base, engine
 Base.metadata.create_all(engine)
+
+from models.student import Student
+from models.course import Course
+
+if __name__ == "__main__":
+    student1 = Student(1, "Tasha", "tasha@email.com")
+    course1 = Course(101, "Python", 3)
+    course2 = Course(102, "Databases", 4)
+
+    student1.enroll(course1)
+    student1.enroll(course2)
+
+    student1.show_courses()
